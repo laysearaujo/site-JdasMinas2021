d3a86b008ad1a1cb9c9c7e02207a6c97823ff72d
Maria Eduarda Lima <meduardalima88@gmail.com>
2021-02-09 23:50:16 -0300

    Ultimas alteraçoes

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -4,7 +4,7 @@
 if(isset($_POST['email'])) {
 	
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "";
+    $email_to = "gamejamdasminas@gmail.com";
     $email_subject = "[GJM 2020] - GAME JAM DAS MINAS -";
  
     
